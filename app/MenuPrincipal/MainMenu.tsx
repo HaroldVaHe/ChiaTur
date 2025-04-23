c228d6c55ba9badad070f165a935c2327aa237ab
david37432 <davidjimenez3743@gmail.com>
2025-04-23 14:12:40 -0500

    Vista Menu Principal

---
+++ app/MenuPrincipal/MainMenu.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { FontAwesome5, MaterialIcons, Entypo, Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+export default function MainMenu() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Image
+  source={require('@/assets/images/DiosaLunaChia.jpg')} // asegúrate de poner la extensión correcta
+  style={styles.avatar}
+/>
+
+      <Text style={styles.title}>ChíaTur</Text>
+      <Text style={styles.subtitle}>¿Qué quieres hacer?</Text>
+
+      <View style={styles.buttonGrid}>
+        <TouchableOpacity style={styles.button} >
+          <FontAwesome5 name="utensils" size={32} color="#FFC107" />
+          <Text style={styles.buttonText}>Gastronomía</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="museum" size={32} color="#FFC107" />
+          <Text style={styles.buttonText}>Cultura</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Entypo name="game-controller" size={32} color="#FFC107" />
+          <Text style={styles.buttonText}>Entretenimiento</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Feather name="shopping-bag" size={32} color="#FFC107" />
+          <Text style={styles.buttonText}>Shopping</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: '#388E3C',
+  },
+  subtitle: {
+    fontSize: 18,
+    marginVertical: 20,
+    color: '#4CAF50',
+  },
+  buttonGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 16,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    width: 130,
+    height: 130,
+    borderRadius: 10,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+});
